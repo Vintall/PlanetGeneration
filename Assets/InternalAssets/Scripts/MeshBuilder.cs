642fdcb9bc44d65fcf26d50631cab24f0d277b68
Vintall <tarantul4884@gmail.com>
2023-05-21 19:15:35 +0300

    Massive mesh generation algorithm improvements. Tried to reuse as much data as possible. But end up using dictionary for noise chunks. So don't know about parallelization.

--- Assets/InternalAssets/Scripts/MeshBuilder.cs
+++ Assets/InternalAssets/Scripts/MeshBuilder.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using Unity.Entities.UniversalDelegates;
 using UnityEngine;
 using static SphereChunk;
 
@@ -67,7 +68,7 @@ public static class MeshBuilder
     const int noiseOctaves = 4;
     const int noiseSpan = 35;
 
-    // BuildPlaneMesh (Unoptimised algorithm) With Vertex Dublication
+    //BuildPlaneMesh(Unoptimised algorithm) With Vertex Dublication
     //public static Mesh BuildPlaneMesh(SphereChunk.ChunkParams chunkParams) // With dublicating points
     //{
     //    Vector3 xVector = chunkParams.xVector;
@@ -146,84 +147,116 @@ public static class MeshBuilder
 
     //    return newMesh;
     //}
-    public static Mesh BuildPlaneMesh(SphereChunk.ChunkParams chunkParams) // With dublicating points
+
+
+
+    public static Mesh BuildPlaneMesh(SphereChunk.ChunkParams chunkParams) // With dublicating points // Optimized
     {
-        Vector3 xVector = chunkParams.xVector;
-        Vector3 yVector = chunkParams.yVector;
+        Vector3 xVector = chunkParams.xVector.normalized;
+        Vector3 yVector = chunkParams.yVector.normalized;
         Vector3 center = chunkParams.chunkCenter;
         Vector2 size = chunkParams.chunkSize;
         float radius = chunkParams.radius;
 
+        Mesh newMesh = new Mesh();
 
-        xVector = xVector.normalized;
-        yVector = yVector.normalized;
         int squarePerAxis = pointsPerAxis - 1;
 
-        Mesh newMesh = new Mesh();
-        List<Vector3> points = new List<Vector3>(squarePerAxis * squarePerAxis * 4);
-        List<Vector3> normals = new List<Vector3>(squarePerAxis * squarePerAxis * 4);
-        List<int> triangles = new List<int>(squarePerAxis * squarePerAxis * 2 * 3);
+        int pointsOnPlane = pointsPerAxis * pointsPerAxis;
+        int squaresOnPlane = squarePerAxis * squarePerAxis;
+
+        int finalVertexCount = squaresOnPlane * 4;
+
+        Vector3[] finalPoints = new Vector3[finalVertexCount];
+        Vector3[] normals = new Vector3[finalVertexCount];
+
+        int[] triangles = new int[squaresOnPlane * 6]; // 6 = 2 triangles per square * 3 vert per triangle
 
         Vector3 basePoint = center - xVector * size.x / 2 - yVector * size.y / 2;
 
+        Vector3[] points = new Vector3[pointsOnPlane];
+        int pointIndex = 0;
 
-        for (int x = 0; x < squarePerAxis; ++x)
-            for (int y = 0; y < squarePerAxis; ++y)
+        Vector3 offsetX = size.x / (pointsPerAxis - 1) * xVector;
+        Vector3 offsetY = size.y / (pointsPerAxis - 1) * yVector;
+
+        for (int x = 0; x < pointsPerAxis; ++x)
+            for (int y = 0; y < pointsPerAxis; ++y, ++pointIndex)
             {
-                Vector3 newPointBase = basePoint;
+                points[pointIndex] = basePoint;
+                points[pointIndex] += x * offsetX;
+                points[pointIndex] += y * offsetY;
+                points[pointIndex] = points[pointIndex].normalized * radius;
+            } // Every point on ideal sphere
 
-                newPointBase += x * size.x / squarePerAxis * xVector;
-                newPointBase += y * size.y / squarePerAxis * yVector;
-
-                Vector3[] squarePoints = new Vector3[4]
-                {
-                    newPointBase,
-                    newPointBase + size.x / squarePerAxis * xVector,
-                    newPointBase + size.y / squarePerAxis * yVector,
-                    newPointBase + size.x / squarePerAxis * xVector + size.y / squarePerAxis * yVector
-                };
-
-                Vector3[] spherePoints = new Vector3[4];
-                Vector3[] realPoints = new Vector3[4];
-
-                int[] vertexIndexes = new int[4];
-
-                for (int i = 0; i < 4; ++i)
-                {
-                    spherePoints[i] = squarePoints[i].normalized * radius;
-
-                    Vector3 finalNoisePoint = spherePoints[i];
-
-                    for (int oct = 0; oct < noiseOctaves; ++oct)
-                    {
-                        float baseNoiseValue = Noise.GeneratePoint(0, noiseSpan / (oct + 1), spherePoints[i] + Vector3.one * 5000);
-                        float modifiedNoiseValue = (0.5f / (oct + 1)) * 0.3f * baseNoiseValue;
-                        finalNoisePoint += spherePoints[i] * modifiedNoiseValue;
-                    }
+        float[][] noiseResults = new float[noiseOctaves][];
 
 
+        float finalKoef = 0.3f * 0.5f;
+        for (int oct = 0; oct < noiseOctaves; ++oct)
+        {
+            noiseResults[oct] = Noise.GeneratePoints(0, noiseSpan / (oct + 1), points);
 
-                    realPoints[i] = finalNoisePoint;
-                    points.Add(realPoints[i]);
-                    vertexIndexes[i] = points.Count - 1;
-                    normals.Add(realPoints[i].normalized);
-                }
+            for (int i = 0; i < points.Length; ++i)
+            {
+                float modifiedNoiseValue = finalKoef / (oct + 1) * noiseResults[oct][i];
+                points[i] += points[i] * modifiedNoiseValue;
+            }
+        }
+        // Next step is just compose squares from those points
 
-                triangles.Add(vertexIndexes[0]);
-                triangles.Add(vertexIndexes[2]);
-                triangles.Add(vertexIndexes[1]);
+        Vector3[,] points2D = new Vector3[pointsPerAxis, pointsPerAxis];
 
-                triangles.Add(vertexIndexes[1]);
-                triangles.Add(vertexIndexes[2]);
-                triangles.Add(vertexIndexes[3]);
+        for (int i = 0; i < pointsPerAxis; ++i)
+            for (int j = 0; j < pointsPerAxis; ++j)
+            {
+                points2D[i, j] = points[i * pointsPerAxis + j];
             }
 
-        newMesh.vertices = points.ToArray();
-        newMesh.triangles = triangles.ToArray();
-        newMesh.normals = normals.ToArray();
+        int finalPointsIter = 0;
+        int trianglesIter = 0;
+        int max = 0;
+        for (int i = 1; i < pointsPerAxis; ++i)
+            for (int j = 1; j < pointsPerAxis; ++j)
+            {
+                normals[finalPointsIter] = points2D[i - 1, j - 1];
+                finalPoints[finalPointsIter++] = points2D[i - 1, j - 1];
+
+                normals[finalPointsIter] = points2D[i, j - 1];
+                finalPoints[finalPointsIter++] = points2D[i, j - 1];
+
+                normals[finalPointsIter] = points2D[i - 1, j];
+                finalPoints[finalPointsIter++] = points2D[i - 1, j];
+
+                normals[finalPointsIter] = points2D[i, j];
+                finalPoints[finalPointsIter++] = points2D[i, j];
+
+
+
+
+                triangles[trianglesIter++] = finalPointsIter - 4;
+                triangles[trianglesIter++] = finalPointsIter - 2;
+                triangles[trianglesIter++] = finalPointsIter - 3;
+
+
+                triangles[trianglesIter++] = finalPointsIter - 2;
+                triangles[trianglesIter++] = finalPointsIter - 1;
+                triangles[trianglesIter++] = finalPointsIter - 3;
+            }
+        Debug.Log(max);
+        newMesh.vertices = finalPoints;
+        newMesh.triangles = triangles; //= new int[triangles.Length];
+        //for (int i = 0; i < triangles.Length; ++i)
+        //    newMesh.triangles[i] = ;
+
+        newMesh.normals = normals;
+
+        //newMesh.RecalculateNormals();
+        //newMesh.RecalculateTangents();
 
         return newMesh;
     }
+
     //public static Mesh BuildPlaneMesh(Vector3 xVector, Vector3 yVector, Vector3 center, Vector2 size, float radius) // With dublicating points
     //{
     //    xVector = xVector.normalized;
--- Assets/InternalAssets/Scripts/Noise.cs
+++ Assets/InternalAssets/Scripts/Noise.cs
@@ -1,77 +1,113 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using Unity.Entities.UniversalDelegates;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public static class Noise
 {
-    public static float[,] GeneratePoints(uint seed, float span, Vector3[] points, )
+    struct NoiseChunk
     {
-        Vector3 basePointOffset = new Vector3(point.x % span, point.y % span, point.z % span);
-        Vector3 basePoint = point - basePointOffset;
+        public Vector3 basePointOffset;
+        public Vector3 basePoint;
+        public Vector3 endPoint;
+        public Vector3[] edgePoints;
+        public Vector3[] noisePoints;
+        public float[] pointsRanges;
+    }
+    public static float[] GeneratePoints(uint seed, int span, Vector3[] points)
+    {
+        float[] result = new float[points.Length];
+        //for (int i = 0; i < points.Length; ++i)
+        //    result[i] = GeneratePoint(seed, span, points[i]);
 
-        Vector3 endPoint = basePoint + Vector3.one * span;
+        //return result;
 
-        Vector3[] edgePoints = new Vector3[]
+        Dictionary<Vector3, NoiseChunk> noiseChunks = new(50);
+        
+        bool isBasePointExist = false;
+
+        void GenerateBasePoints(Vector3 basePointOffset, Vector3 basePoint, Vector3 point)
         {
-            basePoint,
-            new Vector3(endPoint.x, basePoint.y, basePoint.z),
-            new Vector3(basePoint.x, endPoint.y, basePoint.z),
-            new Vector3(basePoint.x, basePoint.y, endPoint.z),
-            new Vector3(endPoint.x, endPoint.y, basePoint.z),
-            new Vector3(endPoint.x, basePoint.y, endPoint.z),
-            new Vector3(basePoint.x, endPoint.y, endPoint.z),
-            endPoint
-        };
-        Vector3[] noisePoints = new Vector3[edgePoints.Length];
-        float[] pointsRanges = new float[edgePoints.Length];
+            NoiseChunk noiseChunk = new NoiseChunk();
+            noiseChunk.basePointOffset = basePointOffset;
+            noiseChunk.basePoint = basePoint;
+            noiseChunk.endPoint = basePoint + Vector3.one * span;
+            noiseChunk.edgePoints = new Vector3[]
+            {
+                noiseChunk.basePoint,
+                new Vector3(noiseChunk.endPoint.x, noiseChunk.basePoint.y, noiseChunk.basePoint.z),
+                new Vector3(noiseChunk.basePoint.x, noiseChunk.endPoint.y, noiseChunk.basePoint.z),
+                new Vector3(noiseChunk.basePoint.x, noiseChunk.basePoint.y, noiseChunk.endPoint.z),
+                new Vector3(noiseChunk.endPoint.x, noiseChunk.endPoint.y, noiseChunk.basePoint.z),
+                new Vector3(noiseChunk.endPoint.x, noiseChunk.basePoint.y, noiseChunk.endPoint.z),
+                new Vector3(noiseChunk.basePoint.x, noiseChunk.endPoint.y, noiseChunk.endPoint.z),
+                noiseChunk.endPoint
+            };
+            Vector3[] localNoisePoints = new Vector3[noiseChunk.edgePoints.Length];
+            //float[] localpointsRanges = new float[edgePoints.Length];
 
-        for (int i = 0; i < edgePoints.Length; ++i)
+            for (int j = 0; j < noiseChunk.edgePoints.Length; ++j)
+            {
+                InitHash((int)noiseChunk.edgePoints[j].x + InitHash((int)noiseChunk.edgePoints[j].y + InitHash((int)noiseChunk.edgePoints[j].z)));
+
+                Vector3 gradientVector = HashVector;
+                localNoisePoints[j] = gradientVector;
+            }
+            noiseChunk.noisePoints = localNoisePoints;
+
+            noiseChunks.Add(basePoint, noiseChunk);
+        }
+        Vector3 currentPoint;
+        for (int i = 0; i < points.Length; ++i)
         {
-            InitHash((int)edgePoints[i].x + InitHash((int)edgePoints[i].y) + InitHash(InitHash((int)edgePoints[i].z)));
+            currentPoint = points[i] + Vector3.one * 5000;
+            Vector3 basePointOffset = new Vector3(currentPoint.x % span, currentPoint.y % span, currentPoint.z % span);
+            Vector3 basePoint = currentPoint - basePointOffset;
 
-            Vector3 gradientVector = HashVector;
-            noisePoints[i] = gradientVector;
-            //pointsRanges[i] = 1 - (point - edgePoints[i]).magnitude / span;
-            //accumulate += noisePoints[i] * pointsRanges[i];
+            if (!noiseChunks.ContainsKey(basePoint))
+                GenerateBasePoints(basePointOffset, basePoint, currentPoint);
+
+            NoiseChunk localNoiseChunk = noiseChunks[basePoint];
+            float[] dotProduct = new float[]
+            {
+            DotGradient(ref localNoiseChunk.noisePoints[0], ref localNoiseChunk.edgePoints[0], ref currentPoint, span),
+            DotGradient(ref localNoiseChunk.noisePoints[1], ref localNoiseChunk.edgePoints[1], ref currentPoint, span),
+            DotGradient(ref localNoiseChunk.noisePoints[2], ref localNoiseChunk.edgePoints[2], ref currentPoint, span),
+            DotGradient(ref localNoiseChunk.noisePoints[3], ref localNoiseChunk.edgePoints[3], ref currentPoint, span),
+            DotGradient(ref localNoiseChunk.noisePoints[4], ref localNoiseChunk.edgePoints[4], ref currentPoint, span),
+            DotGradient(ref localNoiseChunk.noisePoints[5], ref localNoiseChunk.edgePoints[5], ref currentPoint, span),
+            DotGradient(ref localNoiseChunk.noisePoints[6], ref localNoiseChunk.edgePoints[6], ref currentPoint, span),
+            DotGradient(ref localNoiseChunk.noisePoints[7], ref localNoiseChunk.edgePoints[7], ref currentPoint, span)
+            };
+
+            float xRelation = (currentPoint.x - localNoiseChunk.edgePoints[0].x) / span;
+            float yRelation = (currentPoint.y - localNoiseChunk.edgePoints[0].y) / span;
+            float zRelation = (currentPoint.z - localNoiseChunk.edgePoints[0].z) / span;
+
+            xRelation *= xRelation;
+            yRelation *= yRelation;
+            zRelation *= zRelation;
+
+            float firstPlateInterpolation = Interpolate(
+                Interpolate(dotProduct[0], dotProduct[1], xRelation),
+                Interpolate(dotProduct[2], dotProduct[4], xRelation),
+                yRelation);
+
+            float secondPlateInterpolation = Interpolate(
+                Interpolate(dotProduct[3], dotProduct[5], xRelation),
+                Interpolate(dotProduct[6], dotProduct[7], xRelation),
+                yRelation);
+
+            result[i] = Interpolate(firstPlateInterpolation, secondPlateInterpolation, zRelation) / 2 + 0.5f;
         }
 
-        float[] dotProduct = new float[]
-        {
-            DotGradient(ref noisePoints[0], ref edgePoints[0], ref point, span),
-            DotGradient(ref noisePoints[1], ref edgePoints[1], ref point, span),
-            DotGradient(ref noisePoints[2], ref edgePoints[2], ref point, span),
-            DotGradient(ref noisePoints[3], ref edgePoints[3], ref point, span),
-            DotGradient(ref noisePoints[4], ref edgePoints[4], ref point, span),
-            DotGradient(ref noisePoints[5], ref edgePoints[5], ref point, span),
-            DotGradient(ref noisePoints[6], ref edgePoints[6], ref point, span),
-            DotGradient(ref noisePoints[7], ref edgePoints[7], ref point, span)
-        };
-
-        float xRelation = (point.x - edgePoints[0].x) / span;
-        float yRelation = (point.y - edgePoints[0].y) / span;
-        float zRelation = (point.z - edgePoints[0].z) / span;
-
-        xRelation *= xRelation;
-        yRelation *= yRelation;
-        zRelation *= zRelation;
-
-        float firstPlateInterpolation = Interpolate(
-            Interpolate(dotProduct[0], dotProduct[1], xRelation),
-            Interpolate(dotProduct[2], dotProduct[4], xRelation),
-            yRelation);
-
-        float secondPlateInterpolation = Interpolate(
-            Interpolate(dotProduct[3], dotProduct[5], xRelation),
-            Interpolate(dotProduct[6], dotProduct[7], xRelation),
-            yRelation);
-
-        float fullInterpolation = Interpolate(firstPlateInterpolation, secondPlateInterpolation, zRelation);
-
-        return fullInterpolation / 2 + 0.5f;
+        return result;
     }
     public static float GeneratePoint(uint seed, int span, Vector3 point)
     {
+        point += Vector3.one * 5000;
         Vector3 basePointOffset = new Vector3(point.x % span, point.y % span, point.z % span);
         Vector3 basePoint = point - basePointOffset;
 
